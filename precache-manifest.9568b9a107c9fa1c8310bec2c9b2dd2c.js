1b5d43220a416668a1d5411be0a4df896b005e9d
thanhtran1324 <thanhtran1324@gmail.com>
2019-11-11 12:11:50 -0500

    Updates

---
+++ precache-manifest.9568b9a107c9fa1c8310bec2c9b2dd2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c42a47b4ff32e70ca81fde63c6071e7",
+    "url": "/budget_calculator/index.html"
+  },
+  {
+    "revision": "5823b9abe2a9cdc246ab",
+    "url": "/budget_calculator/static/css/main.d12a1485.chunk.css"
+  },
+  {
+    "revision": "13c8b013d6c4864f2c3f",
+    "url": "/budget_calculator/static/js/2.7bc44305.chunk.js"
+  },
+  {
+    "revision": "5823b9abe2a9cdc246ab",
+    "url": "/budget_calculator/static/js/main.00720a09.chunk.js"
+  },
+  {
+    "revision": "9d9de5e6ff0f155d015b",
+    "url": "/budget_calculator/static/js/runtime-main.f7eee6c1.js"
+  }
+]);
